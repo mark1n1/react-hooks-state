2aefd1acdf4c56c86f7c5fce25cc7524893c0e40
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-06-11 18:09:00 -0700

    Completed lecture

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,11 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(count);
     setCount(count + 1);
+    console.log(count);
+    setCount(count + 1);
+    console.log(count);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
